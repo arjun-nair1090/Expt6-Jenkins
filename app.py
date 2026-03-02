9b1564159a66802e2ea537ed5c5198d1f3c0accb
Arjun Nair <arjunnair3006@gmail.com@gmail.com>
2026-03-02 08:57:22 +0530

    niggg

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app= Flask(__name__)
 @app.route("/")
 def index():
-    return "<h1>Arjun Nair</h1>"
+    return "<h1>Arjun Nair</h1><br><h2>AppID=2403953</h2>"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port= 5000, debug= True)
